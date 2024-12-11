a41a3f4f9a1aa4a9e73ac347a4a6714de7d3f918
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2024-12-11 10:40:55 -0500

    auth flow

--- frontend/src/pages/SplashScreen.tsx
+++ frontend/src/pages/SplashScreen.tsx
@@ -1,9 +1,19 @@
 // SplashScreen.js
+import { useEffect } from "react";
 import { useAuth } from "../components/AuthContext";
 import { VerifyBlock } from "../components/Verify";
+import { useNavigate } from "react-router";
 
 const SplashScreen = () => {
-  const { onVerifySuccess } = useAuth();
+  const { isAuthenticated, onVerifySuccess } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      // If authenticated, navigate to the home page or wherever you want users to go after login
+      navigate("/", { replace: true });
+    }
+  }, [isAuthenticated, navigate]);
 
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
