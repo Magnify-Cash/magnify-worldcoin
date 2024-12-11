f0515f6070a05e3d85180091cb2e50a6183f306e
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2024-12-11 11:29:25 -0500

    loading component

--- frontend/src/components/Eruda/index.tsx
+++ frontend/src/components/Eruda/index.tsx
@@ -1,8 +1,6 @@
 import { ReactNode, Suspense, lazy } from "react";
 
-const Eruda = lazy(() =>
-  import("./eruda-provider").then((c) => ({ default: c.Eruda }))
-);
+const Eruda = lazy(() => import("./eruda-provider").then((c) => ({ default: c.Eruda })));
 
 export const ErudaProvider = (props: { children: ReactNode }) => {
   if (process.env.PROD) {
@@ -10,7 +8,16 @@ export const ErudaProvider = (props: { children: ReactNode }) => {
   }
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+          <div className="text-center">
+            <h1 className="text-2xl font-bold mb-4">Loading...</h1>
+            <div className="loader animate-spin w-8 h-8 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+          </div>
+        </div>
+      }
+    >
       <Eruda>{props.children}</Eruda>
     </Suspense>
   );
