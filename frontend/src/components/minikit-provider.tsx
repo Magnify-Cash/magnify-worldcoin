f8eb398893967f07dd8e6cd4337d4d9b94638cc2
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2024-12-11 10:45:22 -0500

    app id

--- frontend/src/components/minikit-provider.tsx
+++ frontend/src/components/minikit-provider.tsx
@@ -3,7 +3,7 @@ import { MiniKit } from "@worldcoin/minikit-js";
 
 export default function MiniKitProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
-    MiniKit.install();
+    MiniKit.install("app_46e97d278d72663d49e73237e429e10f");
     console.log(MiniKit.isInstalled());
   }, []);
 
